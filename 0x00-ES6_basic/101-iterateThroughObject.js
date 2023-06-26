2b87818924ba2e4c7863a7d7960737eb9276ecdd
lekseel <olalekanshogbaike@gmail.com>
2023-06-26 15:09:52 -0700

    jason

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,15 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+  let result = '';
+  let iterator = reportWithIterator[Symbol.iterator]();
+  let item = iterator.next();
+
+  while (!item.done) {
+    result += item.value + ' | ';
+    item = iterator.next();
+  }
+
+  // Remove the trailing ' | ' from the result
+  result = result.slice(0, -3);
+
+  return result;
 }
